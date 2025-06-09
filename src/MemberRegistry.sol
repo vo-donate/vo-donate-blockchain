fb6d53f2ebdfbf36f96b156df52f879852dbc1b1
tjdgml02 <pinedelta@icloud.com>
2025-06-09 21:01:20 +0900

    ✨ member 배열 추가

--- src/MemberRegistry.sol
+++ src/MemberRegistry.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 contract MemberRegistry {
     mapping(address => bool) public isMember;
+    address[] public member;
     address public owner;
 
     constructor() {
@@ -15,10 +16,30 @@ contract MemberRegistry {
     }
 
     function register(address _user) external onlyOwner {
-        isMember[_user] = true;
+        if(!isMember[_user]) {
+            isMember[_user] = true;
+            member.push(_user);
+        }
     }
 
     function unregister(address _user) external onlyOwner {
+        require(isMember[_user], "User not a member");
         isMember[_user] = false;
+
+        for(uint256 i = 0; i < member.length; i++) {
+            if(member[i] == _user) {
+                member[i] = member[member.length -1];
+                member.pop();
+                break;
+            }
+        }
+    }
+
+    function getAllMembers() external view returns (address[] memory) {
+        return member;
+    }
+
+    function getMemberCount() external view returns (uint256) {
+        return member.length;
     }
 }
